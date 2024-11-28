128a322a1412ce1018f37b2fd7b6c4f1431256d2
Kalu <kalu.dematto@gmail.com>
2024-11-28 13:26:58 -0300

    first config for prisma

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Kalu Dematto',
+    email: 'anymail@mail.com',
+  },
+})
