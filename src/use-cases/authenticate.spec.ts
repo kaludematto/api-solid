2305df6d86aff00847c4b3956a69d5864de9a594
Kalu <kalu.dematto@gmail.com>
2024-12-14 18:10:55 -0300

    update tests

--- src/use-cases/authenticate.spec.ts
+++ src/use-cases/authenticate.spec.ts
@@ -29,7 +29,7 @@ describe('Authenticate Use Case', () => {
   })
 
   it('should not be able to authentica with wrong email', async () => {
-    expect(() =>
+    await expect(() =>
       sut.execute({
         email: 'anymail@mail.com',
         password: '123456',
@@ -44,7 +44,7 @@ describe('Authenticate Use Case', () => {
       password_hash: await hash('123456', 6),
     })
 
-    expect(() =>
+    await expect(() =>
       sut.execute({
         email: 'anymail@mail.com',
         password: '123466',
