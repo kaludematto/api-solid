9e63f76774708a61d187ca8ab70a4e8f3c7411e7
Kalu <kalu.dematto@gmail.com>
2024-12-14 18:10:21 -0300

    create checkInsUseCase

---
+++ src/use-cases/check-in.ts
@@ -0,0 +1,27 @@
+import { CheckIn } from '@prisma/client'
+import { CheckInsRepository } from '@/repositories/check-ins-repository'
+
+interface CheckinUseCaseRequest {
+  userId: string
+  gymId: string
+}
+
+interface CheckinUseCaseResponse {
+  checkIn: CheckIn
+}
+
+export class CheckinUseCase {
+  constructor(private checkInsRepository: CheckInsRepository) {}
+
+  async execute({
+    userId,
+    gymId,
+  }: CheckinUseCaseRequest): Promise<CheckinUseCaseResponse> {
+    const checkIn = await this.checkInsRepository.create({
+      gymId,
+      user_id: userId,
+    })
+
+    return { checkIn }
+  }
+}
